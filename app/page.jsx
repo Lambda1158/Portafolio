84eabe3304638cd3686ea62bc9a6d051591ff292
bruno <brunoherrera1158@gmail.com>
2023-09-13 17:06:39 -0300

    update

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className=" mx-auto max-w-[1285px] px-3 overflow-hidden h-screen">
         <Header />
         <div className="mx-auto py-10 h-screen px-5">
-          <TextAnimation text={<Landing />} />
+          <Landing />
         </div>
       </div>
     </section>
