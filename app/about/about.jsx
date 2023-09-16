117abd4a06f3c302b85bddf4245c5f00915f4a2c
bruno <brunoherrera1158@gmail.com>
2023-09-15 22:00:04 -0300

    update

--- app/about/about.jsx
+++ app/about/about.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image"; // Importa el componente Image de Next.js
+import Link from "next/link";
 import Mi from "../../public/myself.jpg";
 const ItemAbout = () => {
   return (
     <div>
-      <h1 className="p-5 text-letra text-5xl">
+      <h1 className="p-5 text-letra text-5xl  font-semibold ">
         Back-end Lover, Full-Stack Capable
       </h1>
       <div className=" p-5 flex justify-between">
@@ -14,13 +15,20 @@ const ItemAbout = () => {
           width={300} // Ancho deseado de la imagen en píxeles
           height={200} // Alto deseado de la imagen en píxeles
         />
-        <p>
-          HELLO I‘M BRUNO HERRERA, I PAY GREAT ATTENTION TO DETAIL IN THE DESIGN
-          AND DEVELOPMENT OF THE PROJECTS. MY PLAN IS TO DEVELOP MY TECHNICAL
-          SKILLS TO BECOME PROFICIENT IN NEW TECHNOLOGIES AND STAY CURRENT WITH
-          EMERGING TRENDS IN MY FIELD. YOU CAN CONTACT ME OR REACH OUT TO ME ON
-          MY SOCIALS.
-        </p>
+        <div className=" p-8">
+          <p className="text-letra font-semibold text-1xl">
+            HELLO I‘M BRUNO HERRERA, I PAY GREAT ATTENTION TO DETAIL IN THE
+            DESIGN AND DEVELOPMENT OF THE PROJECTS. MY PLAN IS TO DEVELOP MY
+            TECHNICAL SKILLS TO BECOME PROFICIENT IN NEW TECHNOLOGIES AND STAY
+            CURRENT WITH EMERGING TRENDS IN MY FIELD. YOU CAN CONTACT ME OR
+            REACH OUT TO ME ON MY SOCIALS.
+          </p>
+          <Link href="/projects">
+            <button className="inline-block font-semibold tracking-[1px] text-[14px] x text-magenta hover:text-letra uppercase py-[12px] px-[32px] uppercase]  bg-letra hover:bg-magenta mt-[15px] xl:mt-[30px] transition duration-100">
+              Projects
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- app/about/page.jsx
+++ app/about/page.jsx
@@ -15,15 +15,6 @@ export default function About() {
             <div className=" mx-auto max-w-[1285px] px-3 text-white overflow-hidden h-screen">
               <Header />
               <div className="mx-auto h-screen px-5 border-white">
-                <Animated
-                  contenido={<h1 className="text-8xl text-center">About me</h1>}
-                />
-
-                <Link href="/projects">
-                  <button className="inline-block font-semibold tracking-[1px] text-[14px] x text-magenta hover:text-letra uppercase py-[12px] px-[32px] uppercase]  bg-letra hover:bg-magenta mt-[15px] xl:mt-[30px] transition duration-100">
-                    Projects
-                  </button>
-                </Link>
                 <TextAnimation text={<ItemAbout />} />
               </div>
             </div>
