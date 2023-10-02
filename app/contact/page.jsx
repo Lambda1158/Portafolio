bd9cb00523d6435735c6afcbd828116bea9ed72d
bruno <brunoherrera1158@gmail.com>
2023-10-02 15:07:12 -0300

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -13,7 +13,7 @@ export default function Contacto() {
             <Header />
             <div className="mx-auto h-screen px-5">
               <h1 className=" indent-6 decoration-8 text-left border-b-4 border-b-letra text-4xl font-light inline-block">
-                👋🏼 Say Hello
+                &apos;👋🏼&apos; Say Hello
               </h1>
               <p className=" py-5 text-2xl font-light decoration-wavy indent-2">
                 If you want to know more about me or my work, send me a message.
