8f76af78a0dc78cac46399dacf5accda26050a5b
bruno <brunoherrera1158@gmail.com>
2023-10-03 16:56:59 -0300

    update

--- app/contact/input.jsx
+++ app/contact/input.jsx
@@ -1,12 +1,14 @@
-const Input = ({ type, id, name, placeholder }) => {
+const Input = ({ type, id, name, placeholder, value, onChange }) => {
   return (
     <input
       className=" h-14  text-2xl bg-letra text-background  rounded-[4px] placeholder:text-background placeholder:font-bold placeholder:px-5"
       type={type}
       id={id}
+      value={value}
       name={name}
       placeholder={placeholder}
       required
+      onChange={onChange}
     />
   );
 };
