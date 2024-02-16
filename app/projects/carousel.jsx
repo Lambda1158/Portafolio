e5800c820e559a5e8f35f837d9b63bd28a9776fa
bruno <brunoherrera1158@gmail.com>
2024-02-16 17:39:30 -0300

    modal ventana

--- app/projects/carousel.jsx
+++ app/projects/carousel.jsx
@@ -1,8 +1,31 @@
 import Image from "next/image";
-
+import { useState } from "react";
+const ImageModal = ({ src, alt, onClose }) => (
+  <div className=" absolute mt-0 ml-0" onClick={onClose}>
+    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+      <img src={src} alt={alt} className="modal-image" onClick={onClose} />
+    </div>
+  </div>
+);
 const Carousel = (data) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const handleImageClick = (src) => {
+    setSelectedImage(src);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div className=" overflow-hidden">
+      {selectedImage && (
+        <ImageModal
+          src={selectedImage}
+          alt="Selected Image"
+          onClose={handleCloseModal}
+        />
+      )}
       <div className="flex justify-start items-center gap-[15px] overflow-x-scroll p-[30px] pb-[30px]">
         {data.data.image.map((Item, Index) => (
           <Image
@@ -11,6 +34,7 @@ const Carousel = (data) => {
             alt={"imagenNumber" + Index}
             width={300}
             height={300}
+            onClick={() => handleImageClick(Item.src)}
           />
         ))}
       </div>
